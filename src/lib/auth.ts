ef5fef2509d55bdfe8007dbb4fa7b1c9bc334644
Tiotedd <andrelsbrito2022@gmail.com>
2023-10-22 15:50:41 -0300

    fix: expect error on google key

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,7 +7,9 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
+      // @ts-expect-error - `clientId` is not a undefined in `.env`
       clientId: process.env.GOOGLE_CLIENT_ID,
+      // @ts-expect-error - `clientId` is not a undefined in `.env`
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ]
