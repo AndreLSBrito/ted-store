7f79573e96fc73f7a5abf5da5e7a3e3c74e9b747
Tiotedd <andrelsbrito2022@gmail.com>
2023-10-22 15:35:55 -0300

    fix: included authOption import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import { AuthOptions} from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prismaClient } from "@/lib/prisma"
