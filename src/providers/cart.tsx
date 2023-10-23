12c029d2f409a4912756d5e8f8af772cba34bb9e
Tiotedd <andrelsbrito2022@gmail.com>
2023-10-23 20:38:35 -0300

    fix: fix error on build project -localstorage no defined

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -36,9 +36,14 @@ export const CartContext = createContext<ICartContext>({
 })
 
 const CartProvider = ({children}: {children: ReactNode}) => {
-  const [products, setProducts] =  useState<CartProduct[]>(
-    JSON.parse(localStorage.getItem("@ted-store/cart-products") || "[]")
-  )
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  
+  useEffect(() => {
+    setProducts(
+      JSON.parse(localStorage.getItem("@fsw-store/cart-products") || "[]"),
+    );
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("@ted-store/cart-products", JSON.stringify(products))
